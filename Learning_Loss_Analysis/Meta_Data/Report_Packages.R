7a7e0c9e4840128439d9215158b00f60aad43200
Adam VanIwaarden <avaniwaarden@gmail.com>
2021-07-05 23:01:33 -0600

    Create Report_Packages.R

---
+++ Learning_Loss_Analysis/Meta_Data/Report_Packages.R
@@ -0,0 +1,68 @@
+#####
+###   Update and install packages required for various analyses, summaries, etc.
+###   included in the LEARNING LOSS report.
+###   Separate sections for CRAN and Github package versions.
+#####
+
+###   Update user's packages
+update.packages(ask = FALSE, checkBuilt = TRUE)
+
+###   Install/update latest packages from GITHUB
+remotes::install_github("centerforassessment/Literasee")
+remotes::install_github("centerforassessment/cfaTools")
+remotes::install_github("centerforassessment/SGPdata")
+remotes::install_github("centerforassessment/SGP")
+remotes::install_github("Rdatatable/data.table")
+remotes::install_github('rstudio/rmarkdown')
+remotes::install_github('rstudio/bookdown')
+remotes::install_github('rstudio/pagedown')
+
+###   Install/update latest packages from CRAN
+
+##    Modeling & Output
+if (!require(MASS)) {
+  install.packages("MASS", dep=T)
+}
+
+if (!require(randomForest)) {
+  install.packages("MASS", dep=T)
+}
+if (!require(caret)) {
+  install.packages("MASS", dep=T)
+}
+if (!require(inTrees)) {
+  install.packages("MASS", dep=T)
+}
+if (!require(modelsummary)) {
+  install.packages("modelsummary", dep=T)
+}
+# if (!require(stargazer)) {
+#   install.packages("stargazer", dep=T)
+# }
+
+#Data Manipulation
+if (!require(reshape2)) {
+  install.packages("reshape2", dep=T)
+}
+if (!require(plyr)) {
+  install.packages("plyr", dep=T)
+}
+
+#Plotting
+if (!require(GGally)) {
+  install.packages("GGally", dep=T)
+}
+if (!require(ggplot2)) {
+  install.packages("ggplot2", dep=T)
+}
+if (!require(plotly)) {
+  install.packages("plotly", dep=T)
+}
+if (!require(vcd)) {
+  install.packages("vcd", dep=T)
+}
+
+#Tables
+if (!require(kableExtra)) {
+  install.packages("kableExtra", dep=T)
+}
